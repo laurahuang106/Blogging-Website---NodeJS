b2c43928d09261f0a78a7aba40c16aa3a888989d
laurahuang106 <huangyuying2018@hotmail.com>
2023-11-20 10:39:30 -0800

    Header update, create about page

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ app.set('view engine', 'ejs')
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs")
+    res.render("index.ejs", {title: "Blogs"})
+})
+
+app.get("/about", (req, res) => {
+    res.render("about.ejs",  {title: "About"})
+})
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs",  {title: "Create blog"})
 })
 
 app.listen(3000)
